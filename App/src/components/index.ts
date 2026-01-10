0f084aa262d9cfa9b75cf72e19111325bb85e809
Soumen Das <sdas721444@gmail.com>
2026-01-11 00:17:15 +0530

    feat: export TransactionDetailModal component in index file

--- App/src/components/index.ts
+++ App/src/components/index.ts
@@ -1,6 +1,7 @@
 export { Button } from './Button';
 export { Card } from './Card';
 export { TransactionItem } from './TransactionItem';
+export { TransactionDetailModal } from './TransactionDetailModal';
 export { LoadingSpinner } from './LoadingSpinner';
 export { EmptyState } from './EmptyState';
 export { SuccessAnimation } from './SuccessAnimation';
